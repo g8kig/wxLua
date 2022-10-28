79a68678eb76d5274dfed1b35c8d0112dc9c8488
Paul <paul.winwood@gmail.com>
2022-10-28 15:35:08 +0100

    Windows build getting closer.

--- buildwx.cmd
+++ buildwx.cmd
@@ -10,12 +10,14 @@ cmake -G"Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=..\install_win
 ninja install
 cd ..
 cd ..
-cd wxlua/wxLua
-if exist build_win && rm -r build_win
+cd wxlua\wxLua
+if exist build_win rm -r build_win
 mkdir build_win
 cd build_win
-cmake -G"Ninja" -DwxWidgets_CONFIG_EXECUTABLE=..\..\..\wxWidgets-3.2.1\build_win\lib\wx\config\msw-unicode-3.2 -DwxLua_LUA_LIBRARY_BUILD_SHARED=OFF -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
+cmake -G"Ninja" -DwxWidgets_CONFIG_EXECUTABLE=..\..\..\wxWidgets-3.2.1\build_win\lib\wx\config\msw-unicode-3.2 -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
 ninja
 cd ..
 cd ..
+cd ..
+
 
--- buildwxlua.cmd
+++ buildwxlua.cmd
@@ -1,9 +1,9 @@
 
 cd wxlua/wxLua
-if exist build_win && rm -r build_win
+if exist build_win rm -r build_win
 mkdir build_win
 cd build_win
-cmake -G"Ninja" -DwxWidgets_CONFIG_EXECUTABLE=..\..\..\wxWidgets-3.2.1\build_win\lib\wx\config\msw-unicode-3.2 -DwxLua_LUA_LIBRARY_BUILD_SHARED=OFF -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
+cmake -G"Ninja" -DwxWidgets_CONFIG_EXECUTABLE=..\..\..\wxWidgets-3.2.1\build_win\lib\wx\config\msw-unicode-3.2 -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
 ninja
 cd ..
 cd ..
