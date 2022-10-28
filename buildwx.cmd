ae0423cb6eac83b01a8a94d12bb6a9c1893c71ba
Paul <paul.winwood@gmail.com>
2022-10-28 17:59:57 +0100

    Windows Build: Copy built DLLs

--- buildwx.cmd
+++ buildwx.cmd
@@ -1,5 +1,6 @@
 
 setlocal
+set CURDIR=%cd%
 cd wxWidgets-3.2.1
 if exist build_win rd /s/q build_win
 if exist install_win rd /s/q  install_win
@@ -14,11 +15,9 @@ cd wxlua/wxLua
 if exist build_win rd /s/q build_win
 mkdir build_win
 cd build_win
-cmake -G"MinGW Makefiles" -DwxWidgets_CONFIGURATION=msw32u -DwxWidgets_LIB_DIR=D:\Source\wxLua\wxWidgets-3.2.1\install_win\lib\gcc_x64_dll -DwxWidgets_ROOT_DIR=D:\Source\wxLua\wxWidgets-3.2.1\install_win -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxWidgets_CONFIGURATION=msw32u -DwxWidgets_LIB_DIR="%CURDIR%/wxWidgets-3.2.1/install_win/lib/gcc_x64_dll" -DwxWidgets_ROOT_DIR="%CURDIR%/wxWidgets-3.2.1/install_win" -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
 mingw32-make.exe -j8
 cd ..
 cd ..
 cd ..
-
-
-
+copy wxWidgets-3.2.1\install_win\lib\gcc_x64_dll\*.dll wxlua\wxLua\build_win\bin\Debug
--- buildwxlua.cmd
+++ buildwxlua.cmd
@@ -1,10 +1,13 @@
 
+set CURDIR=%cd%
+setlocal
 cd wxlua/wxLua
 if exist build_win rd /s/q build_win
 mkdir build_win
 cd build_win
-cmake -G"MinGW Makefiles" -DwxWidgets_CONFIGURATION=msw32u -DwxWidgets_LIB_DIR=D:\Source\wxLua\wxWidgets-3.2.1\install_win\lib\gcc_x64_dll -DwxWidgets_ROOT_DIR=D:\Source\wxLua\wxWidgets-3.2.1\install_win -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxWidgets_CONFIGURATION=msw32u -DwxWidgets_LIB_DIR="%CURDIR%/wxWidgets-3.2.1/install_win/lib/gcc_x64_dll" -DwxWidgets_ROOT_DIR="%CURDIR%/wxWidgets-3.2.1/install_win" -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
 mingw32-make.exe -j8
 cd ..
 cd ..
 cd ..
+copy wxWidgets-3.2.1\install_win\lib\gcc_x64_dll\*.dll wxlua\wxLua\build_win\bin\Debug
