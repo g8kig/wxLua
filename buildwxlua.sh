b5026d49e2d9533557dfd3dd8cc76ccdd9548e1f
Paul Winwood <paul.winwood@sky.com>
2022-10-28 13:48:31 +0100

    Linux build now uses CMAKE

--- buildwxlua.sh
+++ buildwxlua.sh
@@ -2,11 +2,13 @@
 
 set -e
 pushd wxlua/wxLua
-rm -rf build_linux
+[ -d build_linux ] && rm -r build_linux
 mkdir build_linux
 pushd build_linux
-cmake -DwxWidgets_CONFIG_EXECUTABLE=$(pwd)/../../../wxWidgets-3.2.1/linux_build/lib/wx/config/gtk3-unicode-3.2 -DwxLua_LUA_LIBRARY_BUILD_SHARED=OFF -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
-make -j2
+cmake -G"Ninja" -DwxWidgets_CONFIG_EXECUTABLE=$(pwd)/../../../wxWidgets-3.2.1/build_linux/lib/wx/config/gtk3-unicode-3.2 \
+    -DwxLua_LUA_LIBRARY_BUILD_SHARED=OFF -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON \
+    -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
+ninja
 popd
 popd
 
