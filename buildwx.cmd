8263fa43ab1490c34a38275db0fbf363249b3b34
Paul <paul.winwood@gmail.com>
2022-10-29 11:06:29 +0100

    Simplified Windows build files.

--- buildwx.cmd
+++ buildwx.cmd
@@ -1,7 +1,7 @@
 
 setlocal
 set CURDIR=%cd%
-cd wxWidgets-3.2.1
+cd %CURDIR%\wxWidgets-3.2.1
 if exist build_win rd /s/q build_win
 if exist install_win rd /s/q  install_win
 mkdir build_win
@@ -9,15 +9,11 @@ mkdir install_win
 cd build_win
 cmake -G"Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=..\install_win -DwxBUILD_CXX_STANDARD=11 -DwxBUILD_INSTALL=ON -DwxBUILD_SAMPLES=OFF -DwxBUILD_SHARED=ON -DwxBUILD_TOOLKIT=msw -DwxBUILD_VENDOR=custom -DwxUSE_LIBTIFF=OFF -DwxBUILD_PRECOMP=OFF ..
 ninja install
-cd ..
-cd ..
-cd wxlua/wxLua
+cd %CURDIR%\wxlua\wxLua
 if exist build_win rd /s/q build_win
 mkdir build_win
 cd build_win
-cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_CONFIGURATION=msw32u -DwxWidgets_LIB_DIR="%CURDIR%/wxWidgets-3.2.1/install_win/lib/gcc_x64_dll" -DwxWidgets_ROOT_DIR="%CURDIR%/wxWidgets-3.2.1/install_win" -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
+cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_CONFIGURATION=msw32u -DwxWidgets_LIB_DIR="%CURDIR%\wxWidgets-3.2.1\install_win\lib\gcc_x64_dll" -DwxWidgets_ROOT_DIR="%CURDIR%\wxWidgets-3.2.1\install_win" -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
 mingw32-make.exe -j8
-cd ..
-cd ..
-cd ..
-copy wxWidgets-3.2.1\install_win\lib\gcc_x64_dll\*.dll wxlua\wxLua\build_win\bin\Debug
+cd %CURDIR%
+copy %CURDIR%\wxWidgets-3.2.1\install_win\lib\gcc_x64_dll\*.dll wxlua\wxLua\build_win\bin\Debug
--- buildwxluavc.cmd
+++ buildwxluavc.cmd
@@ -1,14 +1,12 @@
 
-set CURDIR=%cd%
 setlocal
-cd wxlua/wxLua
+set CURDIR=%cd%
+cd %CURDIR%\wxlua\wxLua
 if exist build_win rd /s/q build_win
 mkdir build_win
 cd build_win
 cmake -G"Ninja" -DCMAKE_BUILD_TYPE=Release -DwxWidgets_CONFIGURATION=msw32u -DwxWidgets_LIB_DIR="%CURDIR%\wxWidgets-3.2.1\install_win\lib\vc_x64_dll" -DwxWidgets_ROOT_DIR="%CURDIR%\wxWidgets-3.2.1\install_win" -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
 ninja
-cd ..
-cd ..
-cd ..
+cd %CURDIR%
 copy wxWidgets-3.2.1\install_win\lib\vc_x64_dll\*.dll wxlua\wxLua\build_win\bin\Debug
 
