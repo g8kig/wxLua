0f0bbd69f7475f56b3ee61b17a526480a0da37be
Paul <paul.winwood@sky.com>
2022-11-19 20:19:26 +0000

    Fixed Lua 5.3 and 5.4 builds

--- buildwxlua.cmd
+++ buildwxlua.cmd
@@ -7,7 +7,7 @@ mkdir build_win
 cd build_win
 cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release ^
     -DwxWidgets_CONFIGURATION=msw32u -DwxWidgets_LIB_DIR="%CURDIR%\wxWidgets-3.2.1\install_win\lib\gcc_x64_dll" -DwxWidgets_ROOT_DIR="%CURDIR%\wxWidgets-3.2.1\install_win" ^
-    -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 ..
+    -DwxLua_LUA_LIBRARY_BUILD_SHARED=ON -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.4 ..
 if NOT ["%errorlevel%"]==["0"] (
     exit /b %errorlevel%
 )
