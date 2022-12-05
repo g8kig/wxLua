95cca1e14cdb147a79476dada61e2e6d0b47984c
g8kig <paul.winwood@sky.com>
2022-12-05 17:48:36 +0000

    Fixed M1 Mac build

--- buildwxluaosx.sh
+++ buildwxluaosx.sh
@@ -20,8 +20,16 @@ cp $(pwd)/../../../wxWidgets-3.2.1/install_macos/lib/*.dylib $(pwd)/bin/Release/
 cp $(pwd)/../../../wxWidgets-3.2.1/install_macos/lib/*.dylib $(pwd)/bin/Release/Release/
 cp $(pwd)/lib/Release/Release/*.dylib $(pwd)/bin/Release/Release/
 install_name_tool -add_rpath @executable_path/. $(pwd)/bin/Release/Release/wxLua.app/Contents/MacOS/wxLua
+codesign --force --deep --sign - $(pwd)/bin/Release/Release/wxLua.app
 install_name_tool -add_rpath @executable_path/. $(pwd)/bin/Release/Release/wxLuaCan.app/Contents/MacOS/wxLuaCan
+codesign --force --deep --sign - $(pwd)/bin/Release/Release/wxLuaCan.app
 install_name_tool -add_rpath @executable_path/. $(pwd)/bin/Release/Release/wxLuaFreeze.app/Contents/MacOS/wxLuaFreeze
+codesign --force --deep --sign - $(pwd)/bin/Release/Release/wxLuaFreeze.app
 install_name_tool -add_rpath @executable_path/. $(pwd)/bin/Release/Release/lua
+codesign --force --deep --sign - $(pwd)/bin/Release/Release/lua
+for DYLIB in $(ls *.dylib)
+do
+	codesign --force --deep --sign - $DYLIB
+done
 popd
 popd
