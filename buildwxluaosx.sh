d72a8e2f4a39cef7c0661c17cd0666e633c35636
g8kig <paul.winwood@sky.com>
2022-12-16 12:04:30 +0000

    build for both architectures

--- buildwxluaosx.sh
+++ buildwxluaosx.sh
@@ -12,7 +12,7 @@ cmake -G"Xcode" \
     -DwxLua_LUA_LIBRARY_BUILD_SHARED=OFF -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.4 \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release -DBUILD_SHARED_LIBS=FALSE \
     -DCMAKE_SKIP_RPATH=TRUE -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" \
-    -DCMAKE_OSX_ARCHITECTURES=$MACOSX_ARCH -DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOSX_VERSION ..
+    -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.13" ..
 cmake --build .
 cp $(pwd)/../../../wxWidgets-3.2.1/install_macos/lib/*.dylib $(pwd)/bin/Release/Release/wxLua.app/Contents/MacOS/
 cp $(pwd)/../../../wxWidgets-3.2.1/install_macos/lib/*.dylib $(pwd)/bin/Release/Release/wxLuaCan.app/Contents/MacOS/
--- buildwxosx.sh
+++ buildwxosx.sh
@@ -10,7 +10,7 @@ pushd build_macos
 cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_INSTALL_PREFIX=$(pwd)/../install_macos \
     -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
     -DwxBUILD_CXX_STANDARD=11 -DwxBUILD_INSTALL=ON -DwxBUILD_SAMPLES=OFF -DwxBUILD_SHARED=ON -DwxUSE_LIBMSPACK=NO -DwxUSE_LIBTIFF=OFF \
-    -DwxBUILD_TOOLKIT=osx_cocoa ..
+    -DwxBUILD_TOOLKIT=osx_cocoa -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" ..
 cmake --build . --target install
 popd
 popd
@@ -25,7 +25,7 @@ cmake -G"Xcode" \
     -DwxLua_LUA_LIBRARY_BUILD_SHARED=OFF -DwxLua_LUA_LIBRARY_USE_BUILTIN=ON -DwxLua_LUA_LIBRARY_VERSION=5.2 \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release -DBUILD_SHARED_LIBS=FALSE \
     -DCMAKE_SKIP_RPATH=TRUE -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" \
-    -DCMAKE_OSX_ARCHITECTURES=$MACOSX_ARCH -DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOSX_VERSION ..
+    -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.13" ..
 cmake --build .
 cp $(pwd)/../../../wxWidgets-3.2.1/install_macos/lib/*.dylib $(pwd)/bin/Release/Release/wxLua.app/Contents/MacOS/
 cp $(pwd)/../../../wxWidgets-3.2.1/install_macos/lib/*.dylib $(pwd)/bin/Release/Release/wxLuaCan.app/Contents/MacOS/
